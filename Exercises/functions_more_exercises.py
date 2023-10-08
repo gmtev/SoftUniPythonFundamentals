e09efb02c242f8ae67c4a077530fafa1f8a9877a
gmtev <gmtev1007@gmail.com>
2023-10-08 14:39:56 +0300

    Update functions_more_exercises.py

--- Exercises/functions_more_exercises.py
+++ Exercises/functions_more_exercises.py
@@ -15,3 +15,21 @@ def data_type(cmd, inp):
 cmd = input()
 inp = input()
 print(data_type(cmd,inp))
+
+# center point
+from math import floor
+X1 = float(input())
+Y1 = float(input())
+X2 = float(input())
+Y2 = float(input())
+
+
+def cartesian(x1, y1, x2, y2: float):
+    x1, x2 = floor(x1), floor(x2)
+    y1, y2 = floor(y1), floor(y2)
+    return f"({min(x1, x2, key=abs)}, {min(y1, y2, key=abs)})"
+
+
+print(cartesian(X1, Y1, X2, Y2))
+
+
