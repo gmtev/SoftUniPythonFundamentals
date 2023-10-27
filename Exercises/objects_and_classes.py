b2702e7828106991ced66e0236f65ab0ea49a2c1
gmtev <gmtev1007@gmail.com>
2023-10-27 12:19:23 +0300

    Update objects_and_classes.py

--- Exercises/objects_and_classes.py
+++ Exercises/objects_and_classes.py
@@ -67,12 +67,56 @@ class Zoo:
         self.birds = []
 
     def add_animal(self, species, name):
-        pass
+        if species == "mammal":
+            self.mammals.append(name)
+        elif species == "fish":
+            self.fishes.append(name)
+        elif species == "bird":
+            self.birds.append(name)
+        Zoo.__animals += 1
 
     def get_info(self, species):
-        pass
+        message = ""
+        if species == "mammal":
+            message += f"Mammals in {self.name}: {', '.join(self.mammals)}\n"
+        elif species == "fish":
+            message += f"Fishes in {self.name}: {', '.join(self.fishes)}\n"
+        elif species == "bird":
+            message += f"Birds in {self.name}: {', '.join(self.birds)}\n"
+        message += f"Total animals: {Zoo.__animals}"
+        return message
 
 
 zoo_name = input()
 zoo_object = Zoo(zoo_name)
 count_of_animals = int(input())
+for i in range(count_of_animals):
+    species, name = input().split()
+    zoo_object.add_animal(species, name)
+searched = input()
+print(zoo_object.get_info(searched))
+
+
+# Circle
+class Circle:
+    __pi = 3.14
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+        self.radius = diameter / 2
+
+    def calculate_circumference(self):
+        return Circle.__pi * self.diameter
+
+    def calculate_area(self):
+        return Circle.__pi * self.radius * self.radius
+
+    def calculate_area_of_sector(self, angle):
+        return (angle/360) * Circle.__pi * self.radius * self.radius
+
+
+circle = Circle(10)
+angle = 5
+print(f"{circle.calculate_circumference():.2f}")
+print(f"{circle.calculate_area():.2f}")
+print(f"{circle.calculate_area_of_sector(angle):.2f}")
