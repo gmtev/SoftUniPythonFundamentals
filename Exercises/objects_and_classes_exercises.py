8b2119727f57600bf711b1669415d33d6f96c4d7
gmtev <gmtev1007@gmail.com>
2023-10-29 13:30:00 +0200

    Update objects_and_classes_exercises.py

--- Exercises/objects_and_classes_exercises.py
+++ Exercises/objects_and_classes_exercises.py
@@ -46,3 +46,82 @@ class Catalogue:
         to_return = f"Items in the {self.name} catalogue:\n"
         to_return += '\n'.join(sorted(self.products))
         return to_return
+
+
+# Town
+class Town:
+    def __init__(self, name):
+        self.name = name
+        self.latitude = "0°N"
+        self.longitude = "0°E"
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}"
+
+
+# Class
+class Class:
+    __students_count = 22
+
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+        self.grades = []
+
+    def add_student(self, name: str, grade: float):
+        if Class.__students_count > len(self.students):
+            self.students.append(name)
+            self.grades.append(grade)
+
+    def get_average_grade(self):
+        average_grade = (sum(self.grades)/len(self.grades))
+        return float(f'{average_grade:.2f}')
+
+    def __repr__(self):
+        average_grade = self.get_average_grade()
+        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {average_grade}"
+
+
+# Inventory
+class Inventory:
+    def __init__(self, __capacity):
+        self.__capacity = __capacity
+        self.items = []
+
+    def add_item(self, item: str):
+        if self.__capacity > len(self.items):
+            self.items.append(item)
+        else:
+            return "not enough room in the inventory"
+
+    def get_capacity(self):
+        return int(self.__capacity)
+
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {int(self.__capacity)-int(len(self.items))}"
+
+
+# Articles
+class Article:
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+
+    def edit(self, new_content: str):
+        self.content = new_content
+
+    def change_author(self, new_author: str):
+        self.author = new_author
+
+    def rename(self, new_title: str):
+        self.title = new_title
+
+    def __repr__(self):
+        return f"{self.title} - {self.content}: {self.author}"
