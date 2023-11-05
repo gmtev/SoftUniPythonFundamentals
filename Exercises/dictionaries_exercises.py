678251ed36401b3bce7cc8c662ebf809b5d40b44
gmtev <gmtev1007@gmail.com>
2023-11-05 19:21:09 +0200

    Update dictionaries_exercises.py

--- Exercises/dictionaries_exercises.py
+++ Exercises/dictionaries_exercises.py
@@ -27,20 +27,81 @@ for k, v in ores.items():
 # capitals
 countries = input().split(', ')
 capitals = input().split(', ')
-dic = dict(zip(countries, capitals))
+dic = dict(zip(countries, capitals))  # dic = {countries[index]: capitals[index] for index in range(len(countries))}
 for k, v in dic.items():
     print(f'{k} -> {v}')
 
 # phonebook
 phonebook = {}
-cmd = input()
-while len(cmd) > 2:
+while True:
+    cmd = input()
+    if '-' not in cmd:
+        break
     name, phone = cmd.split('-')
     phonebook[name] = phone
-    cmd = input()
 for i in range(int(cmd)):
     contact = input()
     if contact in phonebook.keys():
         print(f'{contact} -> {phonebook[contact]}')
     else:
         print(f"Contact {contact} does not exist.")
+
+# legendary farming
+items = {"shards": 0, "fragments": 0, "motes": 0}
+flag = False
+while not flag:
+    farm_sesh = input().split()
+    for i in range(0, len(farm_sesh), 2):
+        v = int(farm_sesh[i])
+        k = farm_sesh[i+1].lower()
+        if k not in items.keys():
+            items[k] = 0
+        items[k] += int(v)
+        if items['shards'] >= 250:
+            print('Shadowmourne obtained!')
+            items['shards'] -= 250
+            flag = True
+        elif items['fragments'] >= 250:
+            print('Valanyr obtained!')
+            items['fragments'] -= 250
+            flag = True
+        elif items['motes'] >= 250:
+            print('Dragonwrath obtained!')
+            items['motes'] -= 250
+            flag = True
+        if flag:
+            break
+for k,v in items.items():
+    print(f'{k}: {int(v)}')
+
+# force book
+force_sides = {}
+cmd = input()
+while cmd != "Lumpawaroo":
+    if '|' in cmd:
+        side, user = cmd.split(' | ')
+        user_taken = False
+        for i in force_sides.values():
+            if user in i:
+                user_taken = True
+                break
+        if not user_taken:
+            if side not in force_sides.keys():
+                force_sides[side] = []
+            force_sides[side].append(user)
+    elif '->' in cmd:
+        user, side = cmd.split(' -> ')
+        for i in force_sides.values():
+            if user in i:
+                i.remove(user)
+                break
+        if side not in force_sides.keys():
+            force_sides[side] = []
+        force_sides[side].append(user)
+        print(f"{user} joins the {side} side!")
+    cmd = input()
+for side, users in force_sides.items():
+    if len(users) > 0:
+        print(f"Side: {side}, Members: {len(users)}")
+        for user in users:
+            print(f"! {user}")
