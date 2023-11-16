ca4927d364d6b5d0df902205374f09611a517a64
gmtev <gmtev1007@gmail.com>
2023-11-16 17:33:31 +0200

    Create regular_expressions.py

---
+++ Exercises/regular_expressions.py
@@ -0,0 +1,19 @@
+import re
+# match full name
+names = input()
+regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b"
+matches = re.findall(regex, names)
+print(' '.join(matches))
+
+# match phone number
+numbers = input()
+regex = r"\+359-2-\d{3}-\d{4}\b|\+359 2 \d{3} \d{4}\b"
+matches = re.findall(regex, numbers)
+print(', '.join(matches))
+
+# match dates
+dates = input()
+regex = r'(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})'
+matches = re.findall(regex, dates)
+for match in matches:
+    print(f"Day: {match[0]}, Month: {match[2]}, Year: {match[3]}")
