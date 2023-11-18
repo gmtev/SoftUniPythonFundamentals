a529876939b8845821f12f3ba339ae8b43ff6830
gmtev <gmtev1007@gmail.com>
2023-11-18 20:03:52 +0200

    Update regular_expressions.py

--- Exercises/regular_expressions.py
+++ Exercises/regular_expressions.py
@@ -17,3 +17,10 @@ regex = r'(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})'
 matches = re.findall(regex, dates)
 for match in matches:
     print(f"Day: {match[0]}, Month: {match[2]}, Year: {match[3]}")
+
+# match numbers
+input_string = input()
+regex = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+matches = re.finditer(regex, input_string)
+for match in matches:
+    print(match.group(), end=" ")
