172080e5381895f040686a895c743ee26c943a8f
gmtev <gmtev1007@gmail.com>
2023-11-27 16:41:01 +0200

    Update final_exam_preparation_2.py

--- Exercises/final_exam_preparation_2.py
+++ Exercises/final_exam_preparation_2.py
@@ -26,3 +26,39 @@ days = calories // 2000
 print(f"You have food to last you for: {days} days!")
 for match in matches:
     print(f"Item: {match[1]}, Best before: {match[2]}, Nutrition: {match[3]}")
+
+# p!rates
+cities = {}
+info = input().split("||")
+while info[0] != "Sail":
+    name, population, gold = info[0], int(info[1]), int(info[2])
+    if name not in cities.keys():
+        cities[name] = {"population": 0, "gold":0}
+    cities[name]["population"] += population
+    cities[name]["gold"] += gold
+    info = input().split("||")
+event = input().split('=>')
+while event[0] != "End":
+    command = event[0]
+    if command == "Plunder":
+        city_to_plunder, people_killed, gold_looted = event[1], int(event[2]), int(event[3])
+        print(f"{city_to_plunder} plundered! {gold_looted} gold stolen, {people_killed} citizens killed.")
+        cities[city_to_plunder]["population"] -= people_killed
+        cities[city_to_plunder]["gold"] -= gold_looted
+        if cities[city_to_plunder]["population"] == 0 or cities[city_to_plunder]["gold"] == 0:
+            cities.pop(city_to_plunder)
+            print(f"{city_to_plunder} has been wiped off the map!")
+    elif command == "Prosper":
+        town_to_prosper, gold_added = event[1], int(event[2])
+        if gold_added < 0:
+            print("Gold added cannot be a negative number!")
+        else:
+            cities[town_to_prosper]["gold"] += gold_added
+            print(f"{gold_added} gold added to the city treasury. {town_to_prosper} now has {cities[town_to_prosper]['gold']} gold.")
+    event = input().split('=>')
+if cities:
+    print(f"Ahoy, Captain! There are {len(cities)} wealthy settlements to go to:")
+    for city, city_info in cities.items():
+        print(f"{city} -> Population: {city_info['population']} citizens, Gold: {city_info['gold']} kg")
+else:
+    print("Ahoy, Captain! All targets have been plundered and destroyed!")
